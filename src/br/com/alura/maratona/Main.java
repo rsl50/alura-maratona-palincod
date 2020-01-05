c0b122e2f94c21a885f2cc173f8782ca3f7f7f7d
Robson Lima <rsl.robson@gmail.com>
2020-01-05 12:07:09 -0300

    Adicionada função para verificar palindromo

--- src/br/com/alura/maratona/Main.java
+++ src/br/com/alura/maratona/Main.java
@@ -7,9 +7,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int casos = scanner.nextInt();
+        scanner.nextLine();
+
         for (int i = 1; i <= casos; i++) {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
+
             String palavra = scanner.nextLine();
+            if (checkPalindromo(palavra)) {
+                System.out.println("\"YES\"");
+            } else {
+                System.out.println("\"NO\"");
+            }
         }
+     }
+
+    private static boolean checkPalindromo(String palavra) {
+        boolean isPalindromo = true;
+
+        for (int i = 0; i < palavra.length(); i++) {
+            char esquerda = palavra.charAt(i);
+            char direita = palavra.charAt(palavra.length() - 1 - i);
+//            System.out.println(esquerda +" "+ direita);
+            if (esquerda != direita) {
+                isPalindromo = false;
+            }
+        }
+
+        return isPalindromo;
     }
 }
